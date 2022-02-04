774df3f11104ae70f4f475cf81094ee4b3e04a4e
MrTrnr <marietournier.pro@gmail.com>
2022-02-04 15:24:44 +0100

    menu demarrer ok

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("** Administration Collegues **");
+const modulePresenter = require("./presentation.js");
+const presentation = new modulePresenter.Presentation();
+presentation.demarrer();
---
+++ presentation.js
@@ -0,0 +1,8 @@
+class Presentation {
+  constructor() {}
+  demarrer() {
+    console.log("1. Lister les collegues\n99. Sortir");
+  }
+}
+
+exports.Presentation = Presentation;
