f5423db5178f972c86a3b94b5f009125d97e939d
MrTrnr <marietournier.pro@gmail.com>
2022-02-04 17:26:41 +0100

    récuperation de données avec fetch dans service.js ok

--- service.js
+++ service.js
@@ -1,6 +1,15 @@
+//https://formation-angular-collegues.herokuapp.com/api/v1/collegues
+
+import fetch from "node-fetch";
+const response = await fetch(
+  "https://formation-angular-collegues.herokuapp.com/api/v1/collegues"
+);
+
+const r1 = await response.clone();
+
+const results = await Promise.all([response.json(), r1.text()]);
+console.log(results[0]);
+
 class Service {
-  constructor() {
-    import fetch from "node-fetch";
-    //https://formation-angular-collegues.herokuapp.com/api/v1/collegues
-  }
+  constructor() {}
 }
